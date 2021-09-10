4c9aaa5dc8c5c9615660ea9ca501da590b3d35c8
samroodAli <devsamrood@gmail.com>
2021-09-10 15:44:11 +0530

    Component:Create component and create accordion from components

---
+++ src/components/Accordion.js
@@ -0,0 +1,3 @@
+export default function Accordion(){
+  return (<p>Accordion</p>)
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom'
+import App from "./components/App"
 
 const root = document.getElementById('root')
 
-ReactDOM.render(<h1>Hello world</h1>,root)
+ReactDOM.render(<App/>,root)
