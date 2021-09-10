b4cacaa8a779b24d08c80a8bfd7317aa23d1b2da
samroodAli <devsamrood@gmail.com>
2021-09-10 18:40:37 +0530

    State:Use state activeIndex to track active content

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,22 +5,24 @@ const Accordion = ({ items }) => {
 
   const onItemClicked = (index) => {
     setActiveIndex(index);
-    return index;
   };
 
   const renderItems = items.map((item, index) => {
+    const active = activeIndex === index ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onItemClicked(index)}>
+        <div className={`title ${active}`} onClick={() => onItemClicked(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
+
   return <div className="ui styled accordion">{renderItems}</div>;
 };
 
