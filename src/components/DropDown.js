c5be9fc4a6ff35c8532d436a0ee92fddf4e3db81
samroodAli <devsamrood@gmail.com>
2021-09-13 18:56:04 +0530

    Dropdown:Create options from mapping options

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
   return (
     <div>
       <p>Hello world</p>
