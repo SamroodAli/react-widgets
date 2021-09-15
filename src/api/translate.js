4f69a97cc754bce152f505c9fe133ae33f65a879
samroodAli <devsamrood@gmail.com>
2021-09-15 19:35:09 +0530

    Translate:Use debouncer to make less API requests with 500ms time delay

--- src/api/translate.js
+++ src/api/translate.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export default Axios.create({
-  baseURL: `https://translation.googleapis.com/language/translate/v2/`,
+  baseURL: `https://translation.googleapis.com/language/translate/v2`,
   params: {
     key: `AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM`,
   },
--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from "react";
+import googleTranslate from "../api/translate";
 
 const Convert = ({ language, text }) => {
   const [translatedText, setTranslatedText] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
 
   useEffect(() => {
-    setTranslatedText(language.label + text);
-  }, [language, text]);
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await googleTranslate.post(
+        "/",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+          },
+        }
+      );
+      setTranslatedText(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, debouncedText]);
 
   return <h1>{translatedText}</h1>;
 };
