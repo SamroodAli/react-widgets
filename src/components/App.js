5811bfc40e7af62f9203620d354d4e0ff9625837
samroodAli <devsamrood@gmail.com>
2021-09-15 20:12:33 +0530

    Component:Create route component for routing various compents

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,25 @@ import TranslateApp from "./TranslateApp";
 import ColorChooser from "./ColorChooserApp";
 import SearchApp from "./SearchApp";
 import AccordionApp from "./AccordionApp";
-
-const routes = {
-  "/": TranslateApp,
-  "/dropdown": ColorChooser,
-  "/search": SearchApp,
-  "/accordion": AccordionApp,
-};
+import Route from "./route";
 
 const App = () => {
-  return <>{routes[window.location.pathname]()}</>;
+  return (
+    <div>
+      <Route path="/">
+        <TranslateApp />
+      </Route>
+      <Route path="/dropdown">
+        <ColorChooser />
+      </Route>
+      <Route path="/search">
+        <SearchApp />
+      </Route>
+      <Route path="/accordion">
+        <AccordionApp />
+      </Route>
+    </div>
+  );
 };
 
 export default App;
