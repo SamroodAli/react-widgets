c3f35b98cd642c9fd155126fd2cd554692131b1c
samroodAli <devsamrood@gmail.com>
2021-09-15 19:47:32 +0530

    RouterComponent:Create manual routing hash

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import TranslateApp from "./TranslateApp";
+import ColorChooser from "./ColorChooserApp";
+import SearchApp from "./SearchApp";
+import AccordionApp from "./AccordionApp";
+
+const routes = {
+  "/": TranslateApp,
+  "/dropdown": ColorChooser,
+  "/search": SearchApp,
+  "/accordion": AccordionApp,
+};
 
 const App = () => {
-  return (
-    <>
-      <TranslateApp />
-    </>
-  );
+  return <>{routes[window.location.pathname]()}</>;
 };
 
 export default App;
