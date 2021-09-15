f146934cb3172b49ae0ca26daed5d276f7ecbd2d
samroodAli <devsamrood@gmail.com>
2021-09-15 21:02:50 +0530

    Routing:Use window.history.pushState to change current url in the browser

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,7 +1,9 @@
 const Link = ({ href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
+    window.history.pushState({}, "", href);
   };
+
   return (
     <a onClick={onClick} href={href}>
       {children}
