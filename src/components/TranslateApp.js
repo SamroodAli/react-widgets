79d27a6113beb80db5cb2617b56beafd8780014b
samroodAli <devsamrood@gmail.com>
2021-09-15 17:51:15 +0530

    Translate:Create state and input tag for getting user text

--- src/components/TranslateApp.js
+++ src/components/TranslateApp.js
@@ -18,9 +18,16 @@ const options = [
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
 
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <Dropdown
         label="Choose a language"
         options={options}
