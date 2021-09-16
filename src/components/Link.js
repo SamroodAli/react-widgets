ca8f46303f93d25a48069b903879c2a60df9c41d
samroodAli <devsamrood@gmail.com>
2021-09-16 14:02:20 +0530

    Routing:Listen to custom routing for popstate changes(address bar url change)

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,7 @@ const Link = ({ href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
     window.history.pushState({}, "", href);
-    const navEvent = new PopStateEvent("popstatae");
+    const navEvent = new PopStateEvent("popstate");
     window.dispatchEvent(navEvent);
   };
 
--- src/components/route.js
+++ src/components/route.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    window.addEventListener("popstate", () => {
+      console.log("url changed");
+    });
+  }, []);
   return window.location.pathname === path ? children : null;
 };
 
