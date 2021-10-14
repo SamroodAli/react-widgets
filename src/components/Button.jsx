32fcd5cec04e58b3fd3fef2ea708bff23b43973f
samroodAli <devsamrood@gmail.com>
2021-10-14 15:15:21 +0530

    Language:Change submit button text using context

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,8 @@ import LanguageContext from "../contexts/LanguageContext";
 class Button extends React.Component {
   static contextType = LanguageContext;
   render() {
-    // console.log(this.context);
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
