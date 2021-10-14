854f0ba805a6b454be2e58cf630d8d5652bbb8fb
samroodAli <devsamrood@gmail.com>
2021-10-14 13:52:40 +0530

    OnClick:Handlers for the two i tags

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -2,13 +2,28 @@ import React from "react";
 
 class Languages extends React.Component {
   state = { langauge: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ langauge: language });
+  };
+
   render() {
     return (
       <div>
         <div>
           Select a language:
-          <i className="flag us"></i>
-          <i className="flag nl"></i>
+          <i
+            className="flag us"
+            onClick={() => {
+              this.onLanguageChange("english");
+            }}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => {
+              this.onLanguage("dutch");
+            }}
+          ></i>
         </div>
       </div>
     );
