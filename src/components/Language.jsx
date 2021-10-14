2482c5f7005cc4ba434bb2d3f22ab65f3237b469
samroodAli <devsamrood@gmail.com>
2021-10-14 15:59:05 +0530

    Context:Give color context for UserReate component

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Languages extends React.Component {
   state = { language: "english" };
@@ -27,9 +28,11 @@ class Languages extends React.Component {
             }}
           ></i>
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
