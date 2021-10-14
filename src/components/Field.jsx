e9ae59b76c047a272cb37d36763982923521d820
samroodAli <devsamrood@gmail.com>
2021-10-14 15:17:00 +0530

    Context:Consume context in Field component for name

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends React.Component {
+  static contextTypes = LanguageContext;
   render() {
+    const text = this.context === "english" ? "Name" : "Naam";
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
