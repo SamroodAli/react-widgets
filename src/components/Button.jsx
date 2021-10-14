8e472c9c6d1a306b162b4278d7087b96d2f9a332
samroodAli <devsamrood@gmail.com>
2021-10-14 16:06:06 +0530

    Refactor: Refactor color component render prop child button with a render function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import ColorContext from "../contexts/ColorContext";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
   renderSubmit(value) {
     return value === "english" ? "Submit" : "Voorleggen";
   }
-
-  render() {
+  renderButton(color) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {(value) => this.renderSubmit(value)}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
