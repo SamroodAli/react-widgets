9cb862275baf9cb927bdd2ebaa7542ad38adb850
samroodAli <devsamrood@gmail.com>
2021-10-15 14:22:50 +0530

    Context:Use context in Button component to get current language state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,14 @@ import ColorContext from "../contexts/ColorContext";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === "english" ? "Submit" : "Voorleggen";
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Voorleggen";
   }
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
